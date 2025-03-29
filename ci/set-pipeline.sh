2097f6cd2931105b0b27ecdbf95f60f73cd2183d
Mark Alston <marktalston@gmail.com>
2025-03-29 17:51:16 -0600

    Add version flag

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Script to set the Concourse pipeline
 
 if [ -z "$GITHUB_TOKEN" ]; then
@@ -6,41 +6,62 @@ if [ -z "$GITHUB_TOKEN" ]; then
     exit 1
 fi
 
+get_latest_version() {
+    git pull -q --all
+    if ! GIT_RELEASE_TAG=$(git describe --tags "$(git rev-list --tags --max-count=1)" 2>/dev/null); then
+        echo "No release tags found. Make sure to fly the release pipeline."
+        exit 1
+    fi
+    echo "${GIT_RELEASE_TAG##*release-v}"
+}
+
 # These values should be set by the user
 TARGET="main" # Default target name, can be changed by user
 PIPELINE="release-pipeline"
-RELEASE_BODY="Release notes for the new version"
+VERSION=$(get_latest_version)
+RELEASE_BODY=""
 
 usage() {
-    cat << EOF
+    cat <<EOF
 Usage:
-    $0 [-t target] [-u url] [-p pipeline_name]
+    $0 [-t target] [-p pipeline_name] [-v version] [-b release_body]
 
 Options:
-   -t target         Name of the concourse target to use. (default: $TARGET)
+   -t target        Name of the concourse target to use. (default: $TARGET)
 
-   -u url            URL of the Concourse server. (default: $CONCOURSE_URL)
+   -p pipeline_name Name of the pipeline to set. (default: $PIPELINE)
 
-   -p pipeline_name  Name of the pipeline to set. (default: $PIPELINE)
+   -v version       Version of the release. (default: $VERSION)
 
-   -h Help. Displays this message
+   -b release_body  Body of the release notes.
+                    If not provided, it will be empty.
+
+   --help           Show this help message and exit.
 EOF
 }
 
 while [[ $1 =~ ^- && $1 != "--" ]]; do
     case $1 in
-        -t | --target)
-            shift
-            TARGET=$1
-            ;;
-        -p | --pipeline)
-            shift
-            PIPELINE=$1
-            ;;
-        -h | --help)
-            usage
-            exit 0
-            ;;
+    -t | --target)
+        shift
+        TARGET=$1
+        ;;
+    -p | --pipeline)
+        shift
+        PIPELINE=$1
+        ;;
+    -v | --version)
+        shift
+        VERSION=$1
+        ;;
+    -b | --release-body | --body)
+        shift
+        RELEASE_BODY=$1
+        ;;
+    -h | --help)
+        usage
+        exit 0
+        ;;
     esac
     shift
 done
@@ -49,4 +70,4 @@ if [[ $1 == '--' ]]; then shift; fi
 fly -t "$TARGET" set-pipeline -p "$PIPELINE" -c ci/pipeline.yml \
     -v github_token="$GITHUB_TOKEN" \
     -v release_body="$RELEASE_BODY" \
-    -v version="0.1.0"
+    -v version="$VERSION"
